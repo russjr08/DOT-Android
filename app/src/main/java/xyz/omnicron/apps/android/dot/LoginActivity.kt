540253bd693adafa1124d32286962ca3789174b2
Russell <russjr08@gmail.com>
2019-09-17 04:46:21 -0400

    Implemented basic Login Activity
    Greets the user, and prompts to authenticate with Bungie. Setup OAuth Callback handler within Activity.

--- app/src/main/java/xyz/omnicron/apps/android/dot/LoginActivity.kt
+++ app/src/main/java/xyz/omnicron/apps/android/dot/LoginActivity.kt
@@ -1,12 +1,69 @@
 package xyz.omnicron.apps.android.dot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsClient
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_login.*
+import xyz.omnicron.apps.android.dot.api.Destiny
+
+
+
 
 class LoginActivity : AppCompatActivity() {
+
+    var tabConnection: CustomTabsServiceConnection? = null
+
+    var preferences: SharedPreferences = getSharedPreferences("dot", Context.MODE_PRIVATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener {
+            launchTab(this, Uri.parse(Destiny.Constants.LOGIN_ENDPOINT))
+        }
+
+        val openUri = intent.data
+        val code = openUri?.getQueryParameter("code")
+        if (!code.isNullOrBlank()) {
+            Log.d("DOT", "Retrieved OAuth Code: $code")
+
+        }
+
     }
+
+    private fun launchTab(context: Context, uri: Uri) {
+        tabConnection = object : CustomTabsServiceConnection() {
+            override fun onCustomTabsServiceConnected(
+                componentName: ComponentName,
+                client: CustomTabsClient
+            ) {
+                val builder = CustomTabsIntent.Builder()
+                val intent = builder.setToolbarColor(ContextCompat.getColor(baseContext, R.color.colorPrimary))
+                                .setStartAnimations(baseContext, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                                .build()
+                intent.intent.flags = FLAG_ACTIVITY_NEW_TASK // This prevents activity going to background after redirection
+                client.warmup(0L)
+                intent.launchUrl(context, uri)
+            }
+
+            override fun onServiceDisconnected(name: ComponentName) {}
+        }
+        CustomTabsClient.bindCustomTabsService(context, "com.android.chrome", tabConnection)
+    }
+
+    override fun onDestroy() {
+        unbindService(tabConnection as CustomTabsServiceConnection)
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/xyz/omnicron/apps/android/dot/api/Destiny.kt
+++ app/src/main/java/xyz/omnicron/apps/android/dot/api/Destiny.kt
@@ -2,15 +2,21 @@ package xyz.omnicron.apps.android.dot.api
 
 public class Destiny {
 
+
     public class Constants {
 
-        val API_KEY = "ef8699ee753947409bab21607d63c3bb"
+        companion object {
+            val API_KEY = "ef8699ee753947409bab21607d63c3bb"
 
-        val CLIENT_SECRET = "sWw9MbOXK4LlyvOpoCxkiojDP.bQq0NEob-bVBJ1mtM"
-        val CLIENT_ID = "29831"
+            val CLIENT_SECRET = "sWw9MbOXK4LlyvOpoCxkiojDP.bQq0NEob-bVBJ1mtM"
+            val CLIENT_ID = "29831"
 
-        val BASE_URL = "https://www.bungie.net/Platform/Destiny2/"
-        val TOKEN_ENDPOINT = "https://www.bungie.net/Platform/App/OAuth/Token/"
+            val BASE_URL = "https://www.bungie.net/Platform/Destiny2/"
+            val TOKEN_ENDPOINT = "https://www.bungie.net/Platform/App/OAuth/Token/"
+
+            val LOGIN_ENDPOINT = "https://www.bungie.net/en/oauth/authorize?client_id=${CLIENT_ID}&response_type=code"
+        }
+
 
 
 
