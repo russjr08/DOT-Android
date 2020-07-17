7f856073431422e3033388c7bf488f8c62e887fb
Russell Richardson <Russell@KronosAD.com>
2020-07-17 03:18:08 -0400

    Wrap pursuit update in a new thread (to prevent blocking Main UI thread)

--- app/src/main/java/xyz/omnicron/apps/android/dot/api/models/DestinyCharacter.kt
+++ app/src/main/java/xyz/omnicron/apps/android/dot/api/models/DestinyCharacter.kt
@@ -23,70 +23,73 @@ class DestinyCharacter(var membershipId: String,
     fun updatePursuits(destinyApi: Destiny): Completable {
         return Completable.create { subscriber ->
             destinyApi.retrieveCharacterData(this.characterId, listOf(200,202,102,201,301))
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.newThread())
                 .subscribe { data ->
-                    val inventoryNode = data.getJSONObject("inventory")
-                    val dataNode = inventoryNode.getJSONObject("data")
-                    val itemsArray = dataNode.getJSONArray("items")
+                    Thread(Runnable {
+                        val inventoryNode = data.getJSONObject("inventory")
+                        val dataNode = inventoryNode.getJSONObject("data")
+                        val itemsArray = dataNode.getJSONArray("items")
+                        this.pursuits.clear()
 
-                    // Begin parsing basic item entries from API
-                    for(i in 0 until itemsArray.length()) {
-                        val itemNode = itemsArray[i] as JSONObject
-                        if(itemNode.getInt("bucketHash") != 1345459588) {
-                            continue
-                        }
-                        val dbItem = destinyApi.database.getDestinyDatabaseItemFromHash(itemNode.getInt("itemHash"),
-                            "DestinyInventoryItemDefinition")
-                        dbItem?.let { datbaseItem ->
-                            if(itemNode.has("expirationDate")) {
-                                val pursuit = DestinyPursuit(databaseItem = datbaseItem,
-                                    instanceId = itemNode.getString("itemInstanceId"),
-                                    quantity = itemNode.getInt("quantity"),
-                                    expirationDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US).parse(itemNode.getString("expirationDate")),
-                                    bucketHash = itemNode.getInt("bucketHash")
-                                )
-                                this.pursuits.add(pursuit)
+                        // Begin parsing basic item entries from API
+                        for(i in 0 until itemsArray.length()) {
+                            val itemNode = itemsArray[i] as JSONObject
+                            if(itemNode.getInt("bucketHash") != 1345459588) {
+                                continue
+                            }
+                            val dbItem = destinyApi.database.getDestinyDatabaseItemFromHash(itemNode.getInt("itemHash"),
+                                "DestinyInventoryItemDefinition")
+                            dbItem?.let { datbaseItem ->
+                                if(itemNode.has("expirationDate")) {
+                                    val pursuit = DestinyPursuit(databaseItem = datbaseItem,
+                                        instanceId = itemNode.getString("itemInstanceId"),
+                                        quantity = itemNode.getInt("quantity"),
+                                        expirationDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US).parse(itemNode.getString("expirationDate")),
+                                        bucketHash = itemNode.getInt("bucketHash")
+                                    )
+                                    this.pursuits.add(pursuit)
 
-                            } else {
-                                val pursuit = DestinyPursuit(databaseItem = datbaseItem,
-                                    instanceId = itemNode.optString("itemInstanceId"),
-                                    quantity = itemNode.getInt("quantity"),
-                                    expirationDate = null,
-                                    bucketHash = itemNode.getInt("bucketHash")
-                                )
-                                this.pursuits.add(pursuit)
+                                } else {
+                                    val pursuit = DestinyPursuit(databaseItem = datbaseItem,
+                                        instanceId = itemNode.optString("itemInstanceId"),
+                                        quantity = itemNode.getInt("quantity"),
+                                        expirationDate = null,
+                                        bucketHash = itemNode.getInt("bucketHash")
+                                    )
+                                    this.pursuits.add(pursuit)
+
+                                }
 
                             }
-
                         }
-                    }
 
-                    // Begin parsing item components data (such as pursuit objectives)
-                    val componentNode = data.getJSONObject("itemComponents")
-                    val objectivesNode = componentNode.getJSONObject("objectives")
-                    val objectivesDataNode = objectivesNode.getJSONObject("data")
+                        // Begin parsing item components data (such as pursuit objectives)
+                        val componentNode = data.getJSONObject("itemComponents")
+                        val objectivesNode = componentNode.getJSONObject("objectives")
+                        val objectivesDataNode = objectivesNode.getJSONObject("data")
 
-                    for(instanceId in objectivesDataNode.keys()) {
-                        val objectivesArray = (objectivesDataNode[instanceId] as JSONObject).getJSONArray("objectives")
-                        val position = this.pursuits.indexOfFirst { it.instanceId == instanceId }
-                        if(position == -1){
-                            continue
-                        }
-                        for(i in 0 until objectivesArray.length()) {
-                            val objectiveNode = objectivesArray[i] as JSONObject
-                            val objectiveDefinition = destinyApi.database.getDestinyDatabaseObjectiveFromHash(objectiveNode.getInt("objectiveHash"), "DestinyObjectiveDefinition")
-                            if(objectiveDefinition != null) {
-                                this.pursuits[position].objectives.add(DestinyObjectiveData(
-                                    objectiveDefinition = objectiveDefinition,
-                                    progress = objectiveNode.getInt("progress"),
-                                    complete = objectiveNode.getBoolean("complete"),
-                                    visible = objectiveNode.getBoolean("visible")
-                                ))
+                        for(instanceId in objectivesDataNode.keys()) {
+                            val objectivesArray = (objectivesDataNode[instanceId] as JSONObject).getJSONArray("objectives")
+                            val position = this.pursuits.indexOfFirst { it.instanceId == instanceId }
+                            if(position == -1){
+                                continue
                             }
+                            for(i in 0 until objectivesArray.length()) {
+                                val objectiveNode = objectivesArray[i] as JSONObject
+                                val objectiveDefinition = destinyApi.database.getDestinyDatabaseObjectiveFromHash(objectiveNode.getInt("objectiveHash"), "DestinyObjectiveDefinition")
+                                if(objectiveDefinition != null) {
+                                    this.pursuits[position].objectives.add(DestinyObjectiveData(
+                                        objectiveDefinition = objectiveDefinition,
+                                        progress = objectiveNode.getInt("progress"),
+                                        complete = objectiveNode.getBoolean("complete"),
+                                        visible = objectiveNode.getBoolean("visible")
+                                    ))
+                                }
 
+                            }
                         }
-                    }
-                    subscriber.onComplete()
+                        subscriber.onComplete()
+                    }).start()
                 }
         }
     }
