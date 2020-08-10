e7dd774191d981d814fd24fd26b2132a1040e84d
Russell Richardson <Russell@KronosAD.com>
2020-08-09 22:08:34 -0400

    Disabled zero-item graphic for now (Closes #15)
    Hopefully this can be brought back at a later time!

--- app/src/main/java/xyz/omnicron/apps/android/dot/ui/pursuits/PursuitsFragment.kt
+++ app/src/main/java/xyz/omnicron/apps/android/dot/ui/pursuits/PursuitsFragment.kt
@@ -4,7 +4,6 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -141,15 +140,15 @@ class PursuitsFragment : Fragment() {
     }
 
     private fun showListEmptyGraphicIfEmpty() {
-        val mainHandler = Handler(requireContext().mainLooper)
-        val runnable = Runnable {
-            if(getSelectedCharacter().pursuits.size == 0) {
-                nothingLayout.visibility = View.VISIBLE
-            } else {
-                nothingLayout.visibility = View.GONE
-            }
-        }
-        mainHandler.post(runnable)
+//        val mainHandler = Handler(requireContext().mainLooper)
+//        val runnable = Runnable {
+//            if(getSelectedCharacter().pursuits.size == 0) {
+//                nothingLayout.visibility = View.VISIBLE
+//            } else {
+//                nothingLayout.visibility = View.GONE
+//            }
+//        }
+//        mainHandler.post(runnable)
 
     }
 
